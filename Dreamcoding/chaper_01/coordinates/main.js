6997e1cac372a55bc9b2880b9416cc0e039dc0cd
Jaemin <jmeeen@gmail.com>
2021-09-11 21:43:13 +0900

    CSS study, performance enhanced

--- Dreamcoding/chaper_01/coordinates/main.js
+++ Dreamcoding/chaper_01/coordinates/main.js
@@ -5,20 +5,25 @@ const lineH = document.querySelector(".horizontal");
 const linev = document.querySelector(".vertical");
 const target = document.querySelector(".target");
 
-window.addEventListener('mousemove', (event) => {
-  logkey(event);
-});
+//target이 완전히 로드가 되면, 실행해라
+addEventListener('load', () => {
+  window.addEventListener('mousemove', (event) => {
+    logkey(event);
+  });
 
-function logkey(event) {
-  const x = event.clientX;
-  const y = event.clientY;
+  function logkey(event) {
+    const x = event.clientX;
+    const y = event.clientY;
+    const targetRect = target.getBoundingClientRect();
+    const targetHarfWidth = targetRect.width / 2;
+    const targetHarfHeight = targetRect.height / 2;
 
-  tag.innerHTML = `${x}px , ${y}px`
-  tag.style.left = x + 'px';
-  tag.style.top = y + 'px';
-  lineH.style.top = y + 'px';
-  linev.style.left = x + 'px';
-  target.style.left = x + 'px';
-  target.style.top = y + 'px';
-}
+    tag.innerHTML = `${x}px , ${y}px`
 
+    linev.style.transform = `translateX(${x}px)`;
+    lineH.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x - targetHarfWidth}px , ${y - targetHarfHeight}px)`;
+    tag.style.transform = `translate(${x + 20}px , ${y + 20}px)`;
+
+  }
+})
