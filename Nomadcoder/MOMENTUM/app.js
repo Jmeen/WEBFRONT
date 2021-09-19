a9478f5edb62bb6c0600aa9e49288c84fefb6b64
Jaemin <jmeeen@gmail.com>
2021-09-19 13:08:33 +0900

    event document, window

--- Nomadcoder/MOMENTUM/app.js
+++ Nomadcoder/MOMENTUM/app.js
@@ -1,26 +1,45 @@
-'use strict'
-// make caculate
 
-const calculate = {
-  add: function (a, b) {
-    return a + b;
-  },
-  minus: function (a, b) {
-    return a - b;
-  },
-  divide: function (a, b) {
-    return a / b;
-  },
-  times: function (a, b) {
-    return a * b;
-  },
-  power: function (a, b) {
-    return a ** b;
-  }
+const title = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+  console.log("title was clicked!")
+  title.style.color = "blue"
 }
 
-console.log("add(5, 4): " + calculate.add(5, 4))
-console.log("minus(5, 4): " + calculate.minus(5, 4))
-console.log("divide(5, 4): " + calculate.divide(5, 4))
-console.log("times(5, 4): " + calculate.times(5, 4))
-console.log("power(5, 4): " + calculate.power(5, 4))
+console.dir(title)
+function handleMouseEnter() {
+  title.innerText = "Mouse is here!"
+}
+
+function handMouseLeave() {
+  title.innerText = "Mouse is gone!!"
+}
+title.onclick = handleTitleClick;
+// title.addEventListener("click", handleTitleClick);
+title.onmouseenter = handleMouseEnter;
+// title.addEventListener("mouseenter", handleMouseEnter)
+title.addEventListener("mouseleave", handMouseLeave)
+// removeEventLisner를 이용할수 있다. 이게 더 좋을것이다
+
+function resizeWindow() {
+  document.body.style.backgroundColor = "tomato"
+}
+
+function handcleWindowCopy() {
+  alert("copier!!")
+}
+
+function handleWindowOffline() {
+  alert("SOS! no wifi")
+}
+
+function handleWindowOnline() {
+  alert("GOOOOOD!")
+}
+window.addEventListener('resize', resizeWindow)
+
+window.addEventListener('copy', handcleWindowCopy)
+
+window.addEventListener("offline", handleWindowOffline)
+
+window.addEventListener("online", handleWindowOnline)
