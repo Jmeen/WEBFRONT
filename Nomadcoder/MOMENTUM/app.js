114acec5b1e709b8519c5e6a7832aa0521b61021
Jaemin <jmeeen@gmail.com>
2021-09-19 16:22:52 +0900

    preventDefault

--- Nomadcoder/MOMENTUM/app.js
+++ Nomadcoder/MOMENTUM/app.js
@@ -1,83 +1,31 @@
 
-const title = document.querySelector("div.hello:first-child h1");
+// const loginBtn = document.querySelector("#login-form button");
+const loginForm = document.querySelector("#login-form")
+const loginInput = document.querySelector("#login-form input");
 
-function handleTitleClick() {
-  // if (title.style.color == "blue") {
-  //   title.style.color = "tomato";
-  // } else {
-  //   title.style.color = "blue"
+const link = document.querySelector("a")
+
+function handleLinkClick(event) {
+  // 링크라든지, 추가적인 작업을 위해 기본 기능을 막아야 할 경우가 있다.
+  // 이럴떄  preventDefault를 사용한다.
+  event.preventDefault();
+  console.log(event)
+  alert("clicked!")
+}
+link.addEventListener("click", handleLinkClick)
+
+function onLoginSubmit(event) {
+  const userName = loginInput.value;
+  // 브라우저의 리프래쉬를 막는다.
+  event.preventDefault();
+  // if (userName === '') {
+  //   alert("Please wrtite your name")
+  // } else if (userName.length > 15) {
+  //   alert("your name is too long")
   // }
-
-  // const currentColor = title.style.color;
-  // let Newcolor;
-  // if (currentColor == "blue") {
-  //   Newcolor = "tomato";
-  // } else {
-  //   Newcolor = "blue"
-  // }
-  // title.style.color = Newcolor;
-
-  // css의 active 클래스로 변경하여 컬러 변경
-  // 하지만 다른 클래스를 사용하게되면 문제가 생긴다. 
-  // 클래스 이름을 변경하지 말자.
-
-  const clickedclass = "active";
-  // 방법 1 : 클래스의 이름을 찾아서 바꾸고 CSS에서 정리
-  // if (title.className === clickedclass) {
-  //   title.className = ""
-  // } else {
-  //   title.className = clickedclass
-  // }
-
-  // 방법 2 : 만약 클래스중 active라는 애가 있으면 추가/제거
-  // if (title.classList.contains(clickedclass)) {
-  //   // 그걸 삭제해줘
-  //   title.classList.remove(clickedclass);
-  // } else {
-  //   title.classList.add(clickedclass)
-  // }
-
-  // 방법 3: clicked toggle 이용.cssd에 .clicked를 추가하였다.
-  title.classList.toggle("clicked")
+  console.log(event)
 
 
-  // console.log(title.className)
 }
-
-
-
-
-
-
-
-console.dir(title)
-function handleMouseEnter() {
-  title.innerText = "Mouse is here!"
-}
-
-function handMouseLeave() {
-  title.innerText = "Mouse is gone!!"
-}
-title.onclick = handleTitleClick;
-// title.addEventListener("click", handleTitleClick);
-title.onmouseenter = handleMouseEnter;
-// title.addEventListener("mouseenter", handleMouseEnter)
-title.addEventListener("mouseleave", handMouseLeave)
-// removeEventLisner를 이용할수 있다. 이게 더 좋을것이다
-
-function resizeWindow() {
-  document.body.style.backgroundColor = "tomato"
-}
-
-function handcleWindowCopy() {
-  alert("copier!!")
-}
-
-function handleWindowOffline() {
-  alert("SOS! no wifi")
-}
-
-function handleWindowOnline() {
-  alert("GOOOOOD!")
-}
-
+// 브라우저는 submit을 하면 새로고침을 하라고 프로그램 되어있다.
+loginForm.addEventListener('submit', onLoginSubmit)
